f292ac921dfd70836e7eacd729033c7b77eca69f
MohammadShahroudi <149831372+MohammadShahroudi@users.noreply.github.com>
2026-03-06 11:19:49 -0800

    move_working

---
+++ Assets/2D Project/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class Movement : MonoBehaviour
+{
+    // private Rigidbody rb;
+    //private float startSpeed = 3f;
+    // private float moveSpeed = 5f;
+    // public Transform enemy;
+    Vector3 enemyStartPos; 
+    
+    void FixedUpdate()
+    {
+        enemyStartPos = gameObject.transform.position;
+        Rigidbody enemyBody = GetComponent<Rigidbody>();
+        enemyBody.linearVelocity = new Vector3(1f, 0f, 0f) * 1.1f;
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
